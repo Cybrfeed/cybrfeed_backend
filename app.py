b677666ebf5f561d9669fc3fc15b6497eb29c90f
karthik4579 <karthiknair021@gmail.com>
2024-09-07 21:42:15 +0530

    Add CORS fix

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import feedparser
 from pydantic import BaseModel
 from typing import List
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 
 class RSSItem(BaseModel):
     title: str
@@ -12,25 +14,45 @@ class RSSItem(BaseModel):
     description: str
     published: str
 
+
+allowed_origins = [
+    "http://127.0.0.1:8000",
+    "https://cybersec-feed-frontend.onrender.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 def parse_rss_feed(url: str) -> List[RSSItem]:
     feed = feedparser.parse(url)
     items = []
     for entry in feed.entries:
         item = RSSItem(
-            title=entry.get('title', 'No title'),
-            link=entry.get('link', 'No link'),
-            description=entry.get('description', 'No description') if 'description' in entry else 'No description',
-            published=entry.get('published', 'No date')
+            title=entry.get("title", "No title"),
+            link=entry.get("link", "No link"),
+            description=(
+                entry.get("description", "No description")
+                if "description" in entry
+                else "No description"
+            ),
+            published=entry.get("published", "No date"),
         )
         items.append(item)
     return items
+
 
 @app.get("/rss", response_model=List[RSSItem])
 def get_rss_feeds():
     urls = [
         "https://www.darkreading.com/rss.xml",
         "https://feeds.feedburner.com/TheHackersNews",
-        "https://cybersecurityventures.com/feed/"
+        "https://cybersecurityventures.com/feed/",
     ]
     all_items = []
     for url in urls:
@@ -38,5 +60,6 @@ def get_rss_feeds():
         all_items.extend(items)
     return all_items
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000,reload=True)
+    uvicorn.run(app, port=8000)
